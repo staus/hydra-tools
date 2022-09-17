01a4ba92364cc54a89c641c254a6232482a4a175
staus <graphicapark@gmail.com>
2022-09-17 14:33:46 +0200

    Try to log things

--- tools.js
+++ tools.js
@@ -1,37 +1,48 @@
 // register WebMIDI
-navigator.requestMIDIAccess()
+console.log("access")
+console.log(access)
+if (!access) {
+  console.log("No Access)
+	navigator.requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
+}
+else {
+  console.log("Already Access)
+}
 
 function onMIDISuccess(midiAccess) {
-    console.log(midiAccess);
-    var inputs = midiAccess.inputs;
-    var outputs = midiAccess.outputs;
-    for (var input of midiAccess.inputs.values()){
-        input.onmidimessage = getMIDIMessage;
-    }
+  console.log(midiAccess);
+  var inputs = midiAccess.inputs;
+  var outputs = midiAccess.outputs;
+  for (var input of midiAccess.inputs.values()){
+    input.onmidimessage = getMIDIMessage;
+  }
+	access = true
 }
 
 function onMIDIFailure() {
-    console.log('Could not access your MIDI devices.');
+  console.log('Could not access your MIDI devices.');
 }
 
 //create an array to hold our cc values and init to a normalized value
 var controlChange=Array(128).fill(0.5)
 
 getMIDIMessage = function(midiMessage) {
-    var arr = midiMessage.data    
-    var index = arr[1]
-    //console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
-    var val = (arr[2]+1)/128.0  // normalize CC values to 0.0 - 1.0
-    controlChange[index]=val
+  var arr = midiMessage.data    
+  var index = arr[1]
+  //console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
+  var val = (arr[2]+1)/128.0  // normalize CC values to 0.0 - 1.0
+  controlChange[index]=val
 }
 
 function cc(val) {
+  console.log("Set CC")
+  console.log(val)
 	return () => controlChange[val]
 }
 
-console.log(_)
+/*
 if (typeof _ === 'undefined') {
 	_ = new P5({mode: 'WEBGL'})
 }
-console.log(_)
+*/
