fae6faa998f09fef6922790f71e2c3574844b562
staus <graphicapark@gmail.com>
2022-09-17 14:48:45 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -21,7 +21,7 @@ var controlChange=Array(128).fill(0.5)
 getMIDIMessage = function(midiMessage) {
   var arr = midiMessage.data    
   var index = arr[1]
-  //console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
+  console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
   var val = (arr[2]+1)/128.0  // normalize CC values to 0.0 - 1.0
   controlChange[index]=val
 }
