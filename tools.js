b29c49fce312f7c5abe13755d34656711eff7e27
staus <graphicapark@gmail.com>
2022-10-03 01:41:18 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -55,8 +55,7 @@ if(typeof hasRunSetup === 'undefined') {
 	
   navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure)
 
-  _ = new P5({mode: 'WEBGL'})
-  _.pixelDensity(8)
+  _ = new P5({mode: 'WEBGL', width: window.innerWidth * 2, height: window.innerHeight * 2})
 
   hasRunSetup = true
 }
